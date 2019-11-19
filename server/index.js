18fb14d24e1b4d71036e58bb862128f32e15eb0d
Derek Sams <dereksams@dereks-mbp.attlocal.net>
2019-11-19 17:47:47 -0600

    Starting server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const path = require("path"); // Usually moved to the start of file
+const massive = require("massive");
+const express = require("express");
+const session = require("express-session");
+const { SERVER_PORT } = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+massive(process.env.CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+    console.log("database connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on ${SERVER_PORT}`);
+});
--- src/components/JobBoard/JobBoard.js
+++ src/components/JobBoard/JobBoard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./JobBoard.scss";
+import coinbase from "../../Images/coinbase.png";
 
 export default class JobBoard extends Component {
   constructor(props) {
@@ -15,7 +16,12 @@ export default class JobBoard extends Component {
       <div>
         <h1>Find Your Dream Job</h1>
         <div className="each-div">
-          <h1>Coinbase</h1>
+          <div className="company-logo-div">
+            <img src={coinbase} className="company-logo" />
+          </div>
+          <div className="company-name-div">
+            <h1>Coinbase</h1>
+          </div>
         </div>
       </div>
     );
