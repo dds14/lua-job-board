e6c49442b580b60664439df5ee34c7c00446d742
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2020-03-01 13:02:51 -0500

    land as a job: changing text working 🔥🔥

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,46 @@
 import React, { Component } from "react";
 import "./Hero.scss";
 
+const textArray = [
+  "Developer",
+  "Designer",
+  "Data Scientist",
+  "Mobile Developer",
+  "Frontend Engineer",
+  "Beyond"
+];
+
 export default class Hero extends Component {
   constructor() {
     super();
-    this.state = {
-      readytouse: null
-    };
+    this.state = { textIdx: 0 };
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let currentIdx = this.state.textIdx;
+      this.setState({ textIdx: currentIdx + 1 });
+    }, 2000);
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.timeout);
   }
 
   render() {
+    let textThatChanges = textArray[this.state.textIdx % textArray.length];
+
     return (
       <div className="entire-hero-div">
         <div className="hero-left-side-div">
-          <h1>heyyy</h1>
+          <div className="land-a-job-div">
+            <h1 className="land-a-job-as-a-text">Land A Job As A</h1>
+          </div>
+          <div>
+            <h1>
+              <span className="text-that-changes">{textThatChanges}</span>
+            </h1>
+          </div>
         </div>
         <div className="hero-right-side-div">
           <h1>right side</h1>
