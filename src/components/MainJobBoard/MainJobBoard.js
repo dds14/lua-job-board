7cac8ea5c9f0cffbb12a288cb474e5b64fd63752
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2020-03-03 14:31:19 -0500

    val.id setting onClick.

--- src/components/MainJobBoard/MainJobBoard.js
+++ src/components/MainJobBoard/MainJobBoard.js
@@ -8,9 +8,11 @@ export default class MainJobBoard extends Component {
 
     this.state = {
       jobs: [],
-      openDropdown: false
+      openDropdown: false,
+      dropdownId: 0
     };
     this.handleDropdown = this.handleDropdown.bind(this);
+    // this.dropdownId = this.dropdownId.bind(this);
   }
 
   componentDidMount(e) {
@@ -28,36 +30,43 @@ export default class MainJobBoard extends Component {
       <div>
         {this.state.jobs.map((val, index) => {
           return (
-            <div className="job-board-container">
-              <div className="all-jobs" onClick={this.handleDropdown}>
-                <div className="pre-dropdown-container">
-                  <div className="company-logo-div">
-                    <img className="company-logo" src={val.company_logo}></img>
-                  </div>
-                  <div className="title-and-company-div">
-                    <div className="title-div">{val.job_name}</div>
-                    <div className="company-div">
-                      <p className="company-paragraph">{val.company_name}</p>
-                      <p> | </p>
-                      <p className="location-paragraph">
-                        <strong>{val.job_location}</strong>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="days-ago-div">{val.days_since_posted}</div>
-                  <div className="apply-now-div">
-                    <form action={val.company_email} target="_blank">
-                      <button type="submit" className="apply-now-button">
-                        Apply
-                      </button>
-                    </form>
+            // <div className={"myclass-" + val.id}>
+            <div
+              className="all-jobs"
+              onClick={this.handleDropdown}
+              onClick={() => this.setState({ dropdownId: val.id })}
+            >
+              <h1>{this.state.dropdownId}</h1>
+              <div className="pre-dropdown-container">
+                <div className="company-logo-div">
+                  <img className="company-logo" src={val.company_logo}></img>
+                </div>
+                <div className="title-and-company-div">
+                  <div className="title-div">{val.job_name}</div>
+                  <div className="company-div">
+                    <p className="company-paragraph">{val.company_name}</p>
+                    <p> | </p>
+                    <p className="location-paragraph">
+                      <strong>{val.job_location}</strong>
+                    </p>
                   </div>
                 </div>
-                {/* <div className="content"></div> */}
+                <div className="days-ago-div">{val.days_since_posted}</div>
+                <div className="apply-now-div">
+                  <form action={val.company_email} target="_blank">
+                    <button type="submit" className="apply-now-button">
+                      Apply
+                    </button>
+                  </form>
+                </div>
+              </div>
+              {/* <div className="content"></div> */}
+              <div className={"myclass-" + val.id}>
                 {this.state.openDropdown === true ? (
-                  <h1>{val.job_description}</h1>
+                  <h1>{val.id}</h1>
                 ) : (
-                  <h1></h1>
+                  // <h1>true</h1>
+                  <h1>false></h1>
                 )}
               </div>
             </div>
