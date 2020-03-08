e1b917dc22cb76fc8b9b566174de34d80f0199c7
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2020-03-08 16:53:53 -0400

    trying to add analytics

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 import "./App.css";
 import routes from "./routes";
 import Navbar from "./components/Navbar/Navbar";
 import BottomNavbar from "./components/Navbar/BottomNavbar/BottomNavbar";
 import Footer from "./components/Footer/Footer";
 
+const trackingId = "UA-135682867-6";
+// Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 function App() {
   return (
     <HashRouter>
-      <div className="App">
-        <Navbar />
-        <header>{routes}</header>
-        <BottomNavbar />
-        {/* <Footer /> */}
-      </div>
+      <Router history={history}>
+        <div className="App">
+          <Navbar />
+          <header>{routes}</header>
+          <BottomNavbar />
+          {/* <Footer /> */}
+        </div>
+      </Router>
     </HashRouter>
   );
 }
