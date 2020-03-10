21a8e72d74b39819ed84082d4906ab59a10ed928
Jorge Hinojosa <jorge94@live.com>
2020-03-09 21:34:14 -0500

    converted MainJobBoardDisplayPage to function

---
+++ src/components/JobFilters/JobFilters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './JobFilters.scss';
+
+const JobFilters = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default JobFilters
--- src/components/MainJobBoard/MainJobBoardDisplayPage.js
+++ src/components/MainJobBoard/MainJobBoardDisplayPage.js
@@ -1,42 +1,37 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import MainJobBoard from "./MainJobBoard";
 import "./MainJobBoardDisplayPage.scss";
 
-export default class MainJobBoardDisplayPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      readytouse: []
-    };
-  }
+const MainJobBoardDisplayPage = () => {
+  // const [readyToUse, setReadyToUse] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     window.scrollTo(0, 0);
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="entire-job-board-display-page">
-        <h1 className="job-board-title">Job Board</h1>
-        <h2 className="job-board-tagline">
-          🔥 None Of These Jobs Require A CS Degree
-        </h2>
-        <h2 className="job-board-coming-soon">
-          UI/UX and Data Science positions coming soon. Sign up{" "}
-          <a
-            href="https://jobs.trylua.com/devjobs"
-            className="job-board-coming-soon-signup"
-            target="_blank"
-          >
-            here
-          </a>{" "}
-          to be notified when they are added.
-        </h2>
-        <MainJobBoard />
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div className="entire-job-board-display-page">
+      <h1 className="job-board-title">Job Board</h1>
+      <h2 className="job-board-tagline">
+        🔥 None Of These Jobs Require A CS Degree
+      </h2>
+      <h2 className="job-board-coming-soon">
+        UI/UX and Data Science positions coming soon. Sign up{" "}
+        <a
+          href="https://jobs.trylua.com/devjobs"
+          className="job-board-coming-soon-signup"
+          target="_blank"
+        >
+          here
+        </a>{" "}
+        to be notified when they are added.
+      </h2>
+      <MainJobBoard />
+      <Footer />
+    </div>
+  );
 }
+
+export default MainJobBoardDisplayPage;
